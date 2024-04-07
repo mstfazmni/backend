920ab460a2a8622cd23d7a3313dd62fca1b35610
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2024-04-06 22:22:07 -0600

    comment post

--- app.js
+++ app.js
@@ -21,35 +21,35 @@ app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/index.html');
 })
 
-app.post('/register', (req, res) => {
-    const { firstName, lastName, userId, address, status } = req.body;
+// app.post('/register', (req, res) => {
+//     const { firstName, lastName, userId, address, status } = req.body;
     
-    let fee;
-    switch (status) {
-        case 'student':
-            fee = 10;
-            break;
-        case 'staff':
-            fee = 50;
-            break;
-        case 'volunteer':
-            fee = 0;
-            break;
-        default:
-            fee = 0;
-    }
+//     let fee;
+//     switch (status) {
+//         case 'student':
+//             fee = 10;
+//             break;
+//         case 'staff':
+//             fee = 50;
+//             break;
+//         case 'volunteer':
+//             fee = 0;
+//             break;
+//         default:
+//             fee = 0;
+//     }
     
-    const response = {
-        firstName: firstName,
-        lastName: lastName,
-        userId: userId,
-        address: address,
-        status: status,
-        fee: fee
-    };
+//     const response = {
+//         firstName: firstName,
+//         lastName: lastName,
+//         userId: userId,
+//         address: address,
+//         status: status,
+//         fee: fee
+//     };
     
-    res.json(response);
-});
+//     res.json(response);
+// });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
