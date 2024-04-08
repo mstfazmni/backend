1acdd7017ed33a508ad2d48c0f34e8f12e66a14d
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2024-04-07 23:08:03 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
 app.use(bodyParser.json());
@@ -14,42 +13,41 @@ app.use(cors({
     origin: ['*', 'https://mstfazmni.github.io']
   }));
 
-// OPTIONS handler for preflight requests
 app.options('*', cors());
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/index.html');
 })
 
-// app.post('/register', (req, res) => {
-//     const { firstName, lastName, userId, address, status } = req.body;
+app.post('/register', (req, res) => {
+    const { firstName, lastName, userId, address, status } = req.body;
     
-//     let fee;
-//     switch (status) {
-//         case 'student':
-//             fee = 10;
-//             break;
-//         case 'staff':
-//             fee = 50;
-//             break;
-//         case 'volunteer':
-//             fee = 0;
-//             break;
-//         default:
-//             fee = 0;
-//     }
+    let fee;
+    switch (status) {
+        case 'student':
+            fee = 10;
+            break;
+        case 'staff':
+            fee = 50;
+            break;
+        case 'volunteer':
+            fee = 0;
+            break;
+        default:
+            fee = 0;
+    }
     
-//     const response = {
-//         firstName: firstName,
-//         lastName: lastName,
-//         userId: userId,
-//         address: address,
-//         status: status,
-//         fee: fee
-//     };
+    const response = {
+        firstName: firstName,
+        lastName: lastName,
+        userId: userId,
+        address: address,
+        status: status,
+        fee: fee
+    };
     
-//     res.json(response);
-// });
+    res.json(response);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
